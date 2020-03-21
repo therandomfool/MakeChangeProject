baba01266ef7062b5353c90a44a81018db306a08
therandomfool <mddenver@protonmail.com>
2020-03-21 08:27:51 -0700

    Added scanner/ user input

--- src/makechange/MakeChangeApp.java
+++ src/makechange/MakeChangeApp.java
@@ -1,10 +1,20 @@
 package makechange;
 
+import java.util.Scanner;
+
 public class MakeChangeApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+				Scanner sc = new Scanner(System.in);
+				
+				System.out.print("Enter Price of product: ");
+				double price = sc.nextDouble();
+				
+				System.out.print("Enter how much you pay with: ");
+				double paid = sc.nextDouble();
+				
+				double change = (paid-price);
 	}
 
 }
